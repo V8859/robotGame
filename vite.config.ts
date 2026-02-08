aaa510e3fdc7b35cf75094199988eae0401df130
Basil Aziz <sbasilaziz2000@gmail.com>
2026-02-08 23:18:36 +1000

    aaa

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "./",
+  base: "./robotGame/",
   test: {
     environment: "jsdom",
     setupFiles: "./src/Testing/setup.ts",
